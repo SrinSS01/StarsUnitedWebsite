f585a9af6f5a0eeba459613ee0b24cb8753115d3
SrinSS01 <srinjoy.s.01.in@gmail.com>
2023-01-12 22:29:44 +0530

    no comment

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -2,20 +2,30 @@
 
 import React from "react";
 
+const Card = () => (<div className={ "card flex-column w-fit" }>
+    <div className={ "text" }>#TEXT</div>
+    <div className={ "text" } style={{
+        padding: "0 10px",
+        fontSize: 20,
+        inlineSize: 226,
+        overflowWrap: "break-word"
+    }}>#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT</div>
+    <input type={ "button" } value={ "Apply" } className={ "card-button" }/>
+</div>);
+
 export default () => {
     return (<section className="section">
         <div className="text">Analytics</div>
         <div className={ "info" }>
-            <div className={ "card flex-column w-fit" }>
-                <div className={ "text" }>#TEXT</div>
-                <div className={ "text" } style={{
-                    padding: "0 10px",
-                    fontSize: 20,
-                    inlineSize: 226,
-                    overflowWrap: "break-word"
-                }}>#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT#TEXT</div>
-                <input type={ "button" } value={ "Apply" } className={ "card-button" }/>
-            </div>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
     </section>)
 }
